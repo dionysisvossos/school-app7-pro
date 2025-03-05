d2f72e4bf44714697f368bf74d6fe091d0798304
Dionysis.Vossos <dionysis.vossos@windowslive.com>
2025-03-05 19:26:56 +0200

    feat: Teacher DAO interface created

---
+++ src/main/java/gr/aueb/cf/schoolapp/dao/ITeacherDAO.java
@@ -0,0 +1,20 @@
+package gr.aueb.cf.schoolapp.dao;
+
+import gr.aueb.cf.schoolapp.dao.exceptions.TeacherDAOException;
+import gr.aueb.cf.schoolapp.model.Teacher;
+
+import java.util.List;
+
+public interface ITeacherDAO {
+
+    Teacher insert(Teacher teacher) throws TeacherDAOException;
+    Teacher update(Teacher teacher) throws TeacherDAOException;
+    void delete(Integer id) throws TeacherDAOException;
+    Teacher getById(Integer id) throws TeacherDAOException;
+    List<Teacher> getAll() throws TeacherDAOException;
+
+    // Querties
+    Teacher getByUUID(String uuid);
+    Teacher getByLastname(String lastname);
+    Teacher getTeacherByVat(String vat);
+}
